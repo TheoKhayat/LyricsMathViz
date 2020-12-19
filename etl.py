6dcc4d343c4b7657882fb7042a72f62345d5f8cd
Theo Khayat <theokhayat23@gmail.com>
2020-12-19 14:52:34 -0600

    cleanup & blastoff config

--- etl.py
+++ etl.py
@@ -6,7 +6,8 @@ engine = create_engine('postgresql://<USERNAME>:<PASSWORD>@localhost:5432/moar_d
 wordsTable = Table('words', MetaData(), autoload=True, autoload_with=engine)
 db_connection = engine.connect()
 
-def getWordFromWords(getWord):
+
+def getWordFromWords(getWord): # if word exists in Postgres then return record row else None
     result = db_connection.execute(
         select([wordsTable]).where(
             wordsTable.columns.word == getWord)).fetchone()
@@ -31,21 +32,28 @@ def insertIntoWords(thisWord, syllableCount=None, rhymeList=[], searched=False):
                     ,rhyming_words = updatedRhymesList
                     ,datamuse_searched = searched or existingRhymes[3]))               
 
-def getRhymes(word):
+def getRhymes(word): # GET request to datamuse API -> insert/update Postgres
     rhymes = get('https://api.datamuse.com/words?max=1000&rel_rhy=' + word).json()
     rhymeList = [rhyme['word'] for rhyme in rhymes]
     insertIntoWords(word, rhymeList=rhymeList, searched=True)
     for rhyme in rhymes:
         insertIntoWords(rhyme['word'], rhyme['numSyllables'], rhymeList)
 
-def blastOff():
-    wordsToGet = db_connection.execute(
-        select([wordsTable]).where(
-            wordsTable.columns.datamuse_searched == False)).fetchall()
-    #wordsToGet = set(open('/Users/theo/Rhymes/LyricsMathViz/cleaned_data/dr_seuss.txt').read().split())
+def blastOff(artistName=None): # main entrypoint
+    if artistName:
+        localFilePath = f'/Users/theo/Rhymes/LyricsMathViz/cleaned_data/{artistName}.txt'
+        wordsToGet = set(open(localFilePath).read().split())
+    else:
+        wordsToGet = [word[0] for word in db_connection.execute(
+            select([wordsTable]).where(
+                wordsTable.columns.datamuse_searched == False)).fetchall()]
     for word in wordsToGet:
-        getRhymes(word[0])
+        getRhymes(word)
 
-blastOff()
 
-engine.dispose()
+try:
+    blastOff() #blastOff('dr_seuss')
+except Exception as e:
+    print('Something broke...', e)
+finally:
+    engine.dispose()
